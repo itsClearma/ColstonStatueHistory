2def0ae698a67babf5cb0f07159b56b5c22e87f4
clearma <rhiannonnlee@gmail.com>
2026-04-19 00:24:51 +0100

    moreQuotes

--- Assets/Scripts/quotePlacer.cs
+++ Assets/Scripts/quotePlacer.cs
@@ -7,11 +7,14 @@ using TMPro;
 public class quotePlacer : MonoBehaviour
 {
     public string[,] quotesByYear = {
-        {"2020","Statues are about saying 'This was a great man who did great things.' That is not true, he [Colston] was a slave trader and a murderer." ,"Professor David Olusoga", "BBC Article"},
-        {"2020", "it's part of a history that shouldn't be glorified", "Keziah Wenham-Kenyon", "BBC Article"},
-        {"2020", "I never thought I would witness such an empowering moment that I can tell my children about,", "Salma Omar", "BBC Article"},
-        {"2021", "...statues are a symbol of how seriously our cities in Britain are actually taking these issues.", "Dr Shawn Sobers", "BBC Article"},
-        {"2021", "The future of the statue must be decided by the people of Bristol and so I urge everyone to take the opportunity to share their views.", "Marvin Rees", "BBC Article"}
+        {"2018","Having statues of people who oppressed us is not a good thing to be saying to black people in this city." ,"Thangam Debbonaire", "BBC News"},
+        {"2020","Statues are about saying 'This was a great man who did great things.' That is not true, he [Colston] was a slave trader and a murderer." ,"Professor David Olusoga", "BBC News"},
+        {"2020", "it's part of a history that shouldn't be glorified", "Keziah Wenham-Kenyon", "BBC News"},
+        {"2020", "I never thought I would witness such an empowering moment that I can tell my children about,", "Salma Omar", "BBC News"},
+        {"2021", "...statues are a symbol of how seriously our cities in Britain are actually taking these issues.", "Dr Shawn Sobers", "BBC News"},
+        {"2021", "The future of the statue must be decided by the people of Bristol and so I urge everyone to take the opportunity to share their views.", "Marvin Rees", "BBC News"},
+        {"2023", "Edward Colston’s career as a merchant was far darker than many children in his home city of Bristol were taught.","JR Sidhu", "The Clueless Conscience"},
+
     };
 
     [SerializeField] public GameObject Clipping;
